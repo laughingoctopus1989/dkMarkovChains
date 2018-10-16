493d328e10cc0efd96e5a9ccf8681a768e53572d
ronyequest <ronyequest@hunter2.zoom.com>
2018-10-16 09:28:37 -0400

    Committer: ronyequest <ronyequest@hunter2.zoom.com>  On branch testing 	modified:   mchain.py figured  stuff out gotta commit real quick

--- mchain.py
+++ mchain.py
@@ -11,7 +11,21 @@ clear = markov("clear", 3, 2, 1)
 snow = markov("snow", 4, 2, 3)
 def weathergen(A, B, C):
     chain = [[A.name, A.stay, A.go, A.back], [B.name, B.stay, B.go, B.back], [C.name, C.stay, C.go, C.back]]
-    print(chain)
     random.shuffle(chain)
+    i = 0
+    w = 0
+    e = 1
     print(chain)
+    while i != 7:
+        print (chain[w][e])
+        if random.randint(0,chain[w][e]) == chain[w][e]:
+            print(chain[w][0])
+            print("STAY!")
+        elif random.randint(0,chain[w][e]) == chain[w][e+1]:
+            print("GO!")
+        elif random.randint(0,chain[w][e]) == chain[w][e+2]:
+            print("BACK!")
+        else:
+            print("OH NO!")
+        i = i + 1
 weathergen(clear, rain, snow)
