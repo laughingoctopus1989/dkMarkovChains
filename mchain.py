a9ff46d17bbd44dff7357115b33b2b6562499a19
ronyequest <ronyequest@hunter2.zoom.com>
2018-10-15 21:40:56 -0400

    Committer: ronyequest <ronyequest@hunter2.zoom.com> 	modified:   mchain.py figured out a solution to the issue of getting a random start, you can use random.shuffle() to make life ezpz

--- mchain.py
+++ mchain.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 import random
 class markov:
-    def __init__(self, stay, go, back):
+    def __init__(self, name, stay, go, back):
+        self.name = name
         self.stay = stay
         self.go = go
         self.back = back
-rain = markov(2, 4, 3)
-clear = markov(3, 2, 1)
-snow = markov(4, 2, 3)
-def weatherchain(A, B, C):
-    seed = random.randint(0,3)
-    if seed == 1:
-        print(A.stay)
-        start = 0
-    elif  seed == 2:
-        print(B.stay)
-        start = 1
-    elif  seed == 3:
-        print(C.stay)
-        start = 2
-    chain = [A, B, C]
-    print(chain[start])
-
-weatherchain(clear, rain, snow)
+rain = markov("rain", 2, 4, 3)
+clear = markov("clear", 3, 2, 1)
+snow = markov("snow", 4, 2, 3)
+def weathergen(A, B, C):
+    chain = [A.name, B.name, C.name]
+    random.shuffle(chain)
+    print(chain)
+weathergen(clear, rain, snow)
